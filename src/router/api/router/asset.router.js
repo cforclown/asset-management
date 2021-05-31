0c5e821d6dba079d60c48f537cbfa45eb70fc901
JohanSuryanto <johansuryanto@outlook.com>
2021-05-31 13:55:54 +0700

    update asset router

--- src/router/api/router/asset.router.js
+++ src/router/api/router/asset.router.js
@@ -23,6 +23,10 @@ Router.get("/", async (req, res) => {
 
 Router.get("/:assetId", async (req, res) => {
   try {
+    if (!req.params.assetId) {
+      return res.status(400).send("Asset id not found");
+    }
+
     const data = await assetController.GetAssetById(req.params.assetId);
 
     if (!data) {
@@ -40,7 +44,62 @@ Router.get("/:assetId", async (req, res) => {
 
 Router.post("/", async (req, res) => {
   try {
+    if (!req.body.params) {
+      return res.status(400).send("Asset parameter not found");
+    }
+    if (!req.body.params.name) {
+      return res.status(400).send("Name not found");
+    }
+
     const data = await assetController.InsertAsset(req.body);
+
+    if (!data) {
+      return res.send
+        .status(404)
+        .send(global.Response(null, "Asset not found"));
+    }
+
+    res.send(global.Response(data));
+  } catch (err) {
+    global.DumpError(err);
+    res.status(500).send(global.Response(null, err.message));
+  }
+});
+
+Router.put("/", async (req, res) => {
+  try {
+    if (!req.body.params) {
+      return res.status(400).send("Asset parameter not found");
+    }
+    if (!req.body.params._id) {
+      return res.status(400).send("Asset id not found");
+    }
+    if (!req.body.params.name) {
+      return res.status(400).send("Name not found");
+    }
+
+    const data = await assetController.UpdateAsset(req.body);
+
+    if (!data) {
+      return res.send
+        .status(404)
+        .send(global.Response(null, "Asset not found"));
+    }
+
+    res.send(global.Response(data));
+  } catch (err) {
+    global.DumpError(err);
+    res.status(500).send(global.Response(null, err.message));
+  }
+});
+
+Router.delete("/:assetId", async (req, res) => {
+  try {
+    if (!req.params._id) {
+      return res.status(400).send("Asset id not found");
+    }
+
+    const data = await assetController.DeleteAsset(req.params._id);
 
     if (!data) {
       return res.send
